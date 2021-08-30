06e571acad49a535354315ddedfbe50bb89bcef5
Mike Lang <mikelang3000@gmail.com>
2021-08-30 22:05:19 +1000

    basic-ssh: needs dhcp

--- images/basic-ssh/setup.sh
+++ images/basic-ssh/setup.sh
@@ -2,5 +2,5 @@
 echo 'root:spitefully middleweight' | chpasswd
 # Install ssh key
 install -D -m 600 /setup/ssh-key /root/.ssh/authorized_keys
-# Start ssh
-systemctl enable sshd
+# Start sshd and dhcp on boot
+systemctl enable sshd dhcpcd
